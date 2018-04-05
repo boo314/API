2c917dd05e414b58a261bd031e48c8182cecbfa5
Piotr Cichocki <piotr.cichocki@oke.pl>
2018-04-05 20:35:17 +0200

    init

---
+++ AuthService/AuthConnector.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthService
+{
+    public class AuthConnector
+    {
+        public bool Login(string login, string password)
+        {
+            return true;
+        }
+    }
+}
---
+++ WebApi/Controllers/TestController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApi.Controllers
+{
+    public class TestController : ApiController
+    {
+        public string Get(int id, string woot)
+        {
+            return "Get!";
+        }
+
+        public IHttpActionResult Post([FromBody]MojaKlasa model)
+        {
+            return Json(model);
+        }
+
+        public string Delete()
+        {
+            return "Delete!";
+        }
+
+        public string Patch()
+        {
+            return "Patch!";
+        }
+    }
+
+    public class MojaKlasa
+    {
+        public string Woot { get; set; }
+        public int Zoot { get; set; }
+        public List<string> Toot { get; set; }
+    }
+}
